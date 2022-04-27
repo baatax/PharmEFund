d14d570ea18317f9e35bfba52f61fe898a18eaa1
baatax <45241184+baatax@users.noreply.github.com>
2022-04-28 00:40:48 +0200

    Ropsten 0x1CCd53C01E1f10985A023a0Db506b597cA071b0c

--- Bounty_Board.sol
+++ Bounty_Board.sol
@@ -4,25 +4,26 @@ Testing results:
 
 */
 
-//replace block.number with block.timestamp
-//Make it accept stable coins
 //Make it refund to multiple parties
 
 pragma solidity ^0.8.2;
+interface Token {
+    function transfer(address, uint256) external returns(bool);
+    function increaseAllowance(address,uint256) external returns(bool);
+    function transferFrom(address,address,uint256) external returns(bool);
+}
 
 contract Bounty_Board{
-    /*
-    interface Token {
-        function transfer() public;
-    }
+    address _token;
+
     constructor(address token_){
         _token = token_;
     }
-    */
+    
     event Bounty_Posted(address indexed, uint256 indexed,uint256); //Proposer, ID, Costs, Milestones
     event Bounty_Funded(address indexed, uint256 indexed, uint256); // Funder, amount, new voting power
     //event Vote_cast(address indexed, uint256 indexed, uint256, uint256);
-    event Milestone_Unlocked(uint256 indexed, uint256, uint256); //Proposal Id, Block, Milestone Nr
+    event Milestone_Unlocked(uint256 indexed, uint256); //Proposal Id, Block, Milestone Nr
     event Bounty_Refunded(uint256 indexed, address indexed, uint256); // Block where vote ended
 
 
@@ -41,17 +42,20 @@ contract Bounty_Board{
         }
     mapping(uint256 => Bounty) bounties; //Datastructure containing all Bounties
 
-    
-    function Refund_Bounty(uint256 prop_id) public{ 
-        Bounty storage bt = bounties[prop_id];
+    function getBountyend(uint256 bt_id) public view returns(uint64){
+        Bounty storage bt = bounties[bt_id];
+        return bt.start+bt.duration;
+    }
+    function Refund_Bounty(uint256 bt_id) public{ 
+        Bounty storage bt = bounties[bt_id];
         require(bt.funded > 0, "Refund_Bounty: Proposal not funded");
-        require((bt.duration+bt.start) < block.number, "Refund_Bounty: Not yet able to return funds");
+        require((bt.duration+bt.start) < block.timestamp, "Refund_Bounty: Not yet able to return funds");
         require(bt.funders[msg.sender] > 0, "Refund_Bounty: Not a funder");
         uint256 fract = (bt.funders[msg.sender] * 1000000000) / bt.funded;
         uint256 refund = (fract*(bt.funded - bt.paid_out))/100000000;
         bt.funders[msg.sender] = 0; // subtract remaining voting power
-        payable(msg.sender).transfer(refund);
-        emit Bounty_Refunded(prop_id, msg.sender, refund);
+        Token(_token).transfer(msg.sender,refund);
+        emit Bounty_Refunded(bt_id, msg.sender, refund);
     }
 
     function Unlock_Milestone(uint256 prop_id, uint256 msno, address bene) public{
@@ -68,33 +72,38 @@ contract Bounty_Board{
         if(bt.msvotes[msno] > (bt.funded/2)){ // Initiate transfer of funds after milestone quorum is reached
             bt.mspayout[msno] = true;     //Add milestone completed
             bt.paid_out += bt.msvalues[msno-1];     //Add payment to total paid out balance 
-            emit Milestone_Unlocked(prop_id, block.number, msno);
-            payable(bene).transfer(bt.msvalues[msno-1]);
+            emit Milestone_Unlocked(prop_id, msno);
+            Token(_token).transfer(bene, bt.msvalues[msno-1]);
         }
     }
 
-    function Post_Bounty(string calldata _salt, uint64 _duration, uint256 _reward, uint256[] calldata _msvalues) public payable returns(uint256) {
+
+    function Post_Bounty(string calldata _salt, uint64 days_duration, uint256 _reward, uint256[] calldata _msvalues, uint256 stake) public returns(uint256) {
         require(_reward == _sumMilestones(_msvalues), "Post_Bounty: msg.value does not correspond with milestone total");
+        Token(_token).increaseAllowance(address(this),stake);
+        Token(_token).transferFrom(msg.sender, address(this),stake);
         uint256 _id =  uint256(keccak256(abi.encode(msg.sender, _salt))); //Create unique proposal id
         Bounty storage bt = bounties[_id];   //Create new Proposal struct mapped to proposal Id
         bt.reward = _reward;
         bt.msvalues = _msvalues;
-        bt.duration = _duration;
-        bt.start = toUint64(block.number);
-        bt.funded = msg.value;
-        bt.funders[msg.sender] = msg.value;
+        bt.duration = days_duration *60*60*24;
+        bt.start = toUint64(block.timestamp);
+        bt.funded = stake;
+        bt.funders[msg.sender] = stake;
         emit Bounty_Posted(msg.sender, _id,_reward);
 
         return _id;
         }
     
-    function Fund_Bounty(uint256 _id)public payable{
+    function Fund_Bounty(uint256 _id, uint256 amt)public{
         Bounty storage bt = bounties[_id];
         require(bt.reward> bt.funded, "Fund_Bounty: Bounty already fully funded");
-        require(msg.value > 0 && msg.value <= (bt.reward - bt.funded), "Fund_Bounty: No payment");
-        bt.funded += msg.value;
-        bt.funders[msg.sender] += msg.value;
-        emit Bounty_Funded(msg.sender, _id, msg.value);
+        require(amt > 0 && amt <= (bt.reward - bt.funded), "Fund_Bounty: No payment");
+        Token(_token).increaseAllowance(address(this),amt);
+        Token(_token).transferFrom(msg.sender,address(this),amt);
+        bt.funded += amt;
+        bt.funders[msg.sender] += amt;
+        emit Bounty_Funded(msg.sender, _id, amt);
     }
     
     
