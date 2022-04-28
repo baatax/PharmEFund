aaef9b65dc6163c951c1dfa111a935e3f9e4dc96
baatax <45241184+baatax@users.noreply.github.com>
2022-04-28 18:23:55 +0200

    Updated allowance contract
    Takes tx.origin instead of msg.sender to prevent having to give permission first before interacting with smart contracts

--- Pharma_Token.sol
+++ Pharma_Token.sol
@@ -5,17 +5,29 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+//Add override function to change increaseAllowance to use tx.origin instead of msg.sender
+
 contract PharmaToken is ERC20, ERC20Burnable, Ownable {
     constructor() ERC20("PharmaToken", "PTK") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-
+    function Exchange_Tokens(uint256 amt) public{
+        _burn(msg.sender, amt);
+        payable(msg.sender).transfer(amt);
+    }
     function Buy_Tokens() public payable{
         _mint(msg.sender, msg.value);
     }
-    function empty_funds() public onlyOwner{
-        payable(msg.sender).transfer(address(this).balance);
+    function empty_funds(uint256 amount) public onlyOwner{
+        payable(msg.sender).transfer(amount);
     }
+    
+    function increaseAllowance(address spender, uint256 addedValue) public override returns (bool) {
+        address owner = tx.origin;
+        _approve(owner, spender, allowance(owner, spender) + addedValue);
+        return true;
+    }
+    
 }
